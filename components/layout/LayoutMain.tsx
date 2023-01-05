98520936521dbfae8cab74c956a64fd265e01cf6
Tonatiuj Sánchez LP <tonsanzjimz@gmail.com>
2023-01-05 15:18:56 -0600

    Layout principal añadido

---
+++ components/layout/LayoutMain.tsx
@@ -0,0 +1,28 @@
+import Head from "next/head"
+import { FC, ReactNode } from "react"
+
+interface Props {
+    children: ReactNode
+    namePage: string
+}
+
+export const LayoutMain:FC<Props> = ({ children, namePage }) => {
+    return (
+        <>
+            <Head>
+              <title>Café - { namePage }</title>
+              <meta name="description" content="Cafetería" />
+              <meta name="viewport" content="width=device-width, initial-scale=1" />
+              <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className="md:flex">
+                <aside className="md:w-3/12 xl:w-1/4 2xl:1/5">
+                    <h2>Siderbar here</h2>
+                </aside>
+                <main className="md:w-7/12 xl:w-3/4 2xl:4/5 h-screen overflow-y-auto">
+                    { children }
+                </main>
+            </div>
+        </>
+    )
+}
